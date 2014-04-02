65d54cefdd396d271b0340dba71e0401bde74125
47tibo <47tibo@gmail.com>
2014-04-02 14:30:29 +0200

    done time carousel

--- www/js/index.js
+++ www/js/index.js
@@ -86,12 +86,12 @@ define('index', [
     var loadTemplates = function () {
 
         require([
-            'text!../tpl/search_club_schedule.tpl.html'
+            //'text!../tpl/search_club_schedule.tpl.html'
             //'text!../tpl/search_club_activity.tpl.html'
             //'text!../tpl/search_activity_schedule.tpl.html'
             //'text!../tpl/search_activity.tpl.html'
             //'text!../tpl/activity.tpl.html'
-           //'text!../tpl/club.tpl.html'
+           'text!../tpl/club.tpl.html'
         //'text!../tpl/search_club.tpl.html'
         //'text!../tpl/home.tpl.html'
         ], buildHome);
