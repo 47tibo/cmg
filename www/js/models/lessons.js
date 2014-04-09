5fba3eda5de331780a013247fd888f3ad931dcb3
47tibo <47tibo@gmail.com>
2014-04-09 16:32:01 +0200

    done activities scheduel filter

--- www/js/models/lessons.js
+++ www/js/models/lessons.js
@@ -1,4 +1,4 @@
-;define('models/lessons', ['utils'], (function( Lessons ){
+;define('models/lessons', ['utils'], (function( Utils ){
 
     function Lessons( lessons ) {
         this._L = lessons.slice( 0 );
@@ -8,6 +8,43 @@
 
         get: function get () {
             return this._L;
+        },
+
+        formatDistance: function formatDistance( distance ) {
+           // possible formats are "2.60 km", "26.4 km", "1.63 km", "630 m"
+           // distance is a number
+           var distanceChunks;
+           distance = distance + '';
+           distanceChunks = distance.split('');
+           if ( distanceChunks[ 0 ] === '0' ) {
+                // " 0.630 " ie "630 m"
+                distance = distanceChunks.splice( 2, 3 ).join('') + ' m';
+           } else {
+            // "2.60 km", "26.4 km", "1.63 km", simply keep 1st 4 char
+                distance = distanceChunks.splice( 0, 4 ).join('') + ' km';
+           }
+           return distance;
+        },
+
+        // create a "distance" field for each club & sort on it
+        sortByLocation: function sortByLocation( fn ) {
+            var self = this;
+
+            Utils.onCurrentPosition( function( coords ){
+                var clubLat, clubLong, distance;
+                for (var i = 0, l = self._L.length; i < l; i+=1) {
+                    clubLat = self._L[ i ].latitude;
+                    clubLong = self._L[ i ].longitude;
+                    distance = Utils.distance( coords.lon, coords.lat, clubLong, clubLat ); //km
+                    self._L[ i ].distance = distance;
+                    // store a pretty distance for display
+                    self._L[ i ][ 'distance_formated' ] = self.formatDistance( self._L[ i ].distance );
+                }
+                self._L.sort( function( a, b ) {
+                    return a.distance - b.distance;
+                });
+                fn.call( self );
+            });
         },
 
         // sort by name, type
